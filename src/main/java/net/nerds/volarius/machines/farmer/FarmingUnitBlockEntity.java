027602e08d1500a0413ef30a0a7a31a5c28b7043
Kyler <kyler.love@gmail.com>
2019-06-07 22:48:19 -0400

    farmer works

--- src/main/java/net/nerds/volarius/machines/farmer/FarmingUnitBlockEntity.java
+++ src/main/java/net/nerds/volarius/machines/farmer/FarmingUnitBlockEntity.java
@@ -1,27 +1,42 @@
 package net.nerds.volarius.machines.farmer;
 
 import net.fabricmc.fabric.api.block.entity.BlockEntityClientSerializable;
+import net.minecraft.block.Block;
+import net.minecraft.block.CropBlock;
+import net.minecraft.block.FarmlandBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.Inventory;
+import net.minecraft.item.AliasedBlockItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.DefaultedList;
 import net.minecraft.util.Tickable;
+import net.minecraft.util.math.BlockPos;
 import net.nerds.volarius.machines.MachineBlockEntities;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class FarmingUnitBlockEntity extends BlockEntity implements Tickable, BlockEntityClientSerializable, Inventory {
 
+    Logger logger = LogManager.getLogger();
+
     private int tickCounter = 0; //counter to validate if waited
-    private int tickValidator = 20; //how many ticks to wait
+    private int tickValidator = 60; //how many ticks to wait
+    private int farmingRange = 3; //blocks range away from farming unit
+    private int maxStorage = 54;
     public DefaultedList<ItemStack> inventory;
 
     public FarmingUnitBlockEntity() {
         super(MachineBlockEntities.FARMING_UNIT_ENTITY);
-        inventory = DefaultedList.create(54, ItemStack.EMPTY);
+        inventory = DefaultedList.create(maxStorage, ItemStack.EMPTY);
     }
 
     @Override
@@ -37,7 +52,7 @@ public class FarmingUnitBlockEntity extends BlockEntity implements Tickable, Blo
     @Override
     public void fromTag(CompoundTag nbt) {
         super.fromTag(nbt);
-        inventory = DefaultedList.create(54, ItemStack.EMPTY);
+        inventory = DefaultedList.create(maxStorage, ItemStack.EMPTY);
         Inventories.fromTag(nbt, this.inventory);
     }
 
@@ -52,7 +67,7 @@ public class FarmingUnitBlockEntity extends BlockEntity implements Tickable, Blo
     public void tick() {
         if(tickCounter >= tickValidator) {
             tickCounter = 0;
-            System.out.println("try to farm");
+            attemptHarvest();
         } else {
             tickCounter++;
         }
@@ -108,5 +123,66 @@ public class FarmingUnitBlockEntity extends BlockEntity implements Tickable, Blo
     @Override
     public void clear() {
         inventory.clear();
+    }
+
+    /**
+     * Called during tick().
+     * attempts to harvest grown CropBlocks and will add drops to inventory and replant with CropBlock seed
+     */
+    private void attemptHarvest() {
+        if(!world.isClient) {
+            List<ItemStack> drops = new ArrayList<>();
+            Iterable<BlockPos> farmingArea = BlockPos.iterate(new BlockPos(pos.getX() - farmingRange, pos.getY(), pos.getZ() - farmingRange),
+                    new BlockPos(pos.getX() + farmingRange, pos.getY(), pos.getZ() + farmingRange));
+            for(BlockPos blockPos : farmingArea) {
+                Block block = world.getBlockState(blockPos).getBlock();
+                if(world.getBlockEntity(pos) != null && block instanceof CropBlock) {
+                    CropBlock cropBlock = (CropBlock) world.getBlockState(blockPos).getBlock();
+                    if(cropBlock.isMature(world.getBlockState(blockPos))) {
+                        //get drops from crop
+                        drops = CropBlock.getDroppedStacks(world.getBlockState(blockPos), (ServerWorld) world, blockPos, null);
+                        //add drops to inventory (-1 seedItem)
+                        AliasedBlockItem seedsFromPlant = addItemsToInventory(drops);
+                        //attempt to replant with seed
+                        replantSeed(seedsFromPlant, blockPos);
+                    }
+                }
+            }
+        }
+    }
+
+    private AliasedBlockItem addItemsToInventory(List<ItemStack> itemStackList) {
+        AliasedBlockItem aliasedBlockItems = null;
+        for(ItemStack itemStack : itemStackList) {
+            //remove a seed from stack for planting
+            if((itemStack.getItem() instanceof AliasedBlockItem && ((AliasedBlockItem) itemStack.getItem()).getBlock() instanceof CropBlock)) {
+                aliasedBlockItems = (AliasedBlockItem) itemStack.getItem();
+                itemStack.subtractAmount(1);
+            }
+            //loop through inventory looking for space
+            for(int i = 0; i < inventory.size(); i++) {
+                if(inventory.get(i).isEmpty() && itemStack.getAmount() > 0) {
+                    inventory.set(i, itemStack);
+                    markDirty();
+                    break;
+                } else if(inventory.get(i).isEqualIgnoreTags(itemStack) &&
+                        (inventory.get(i).getAmount() + itemStack.getAmount() < 64 &&
+                                itemStack.getAmount() > 0)
+                ) {
+                    inventory.set(i, new ItemStack(itemStack.getItem(), itemStack.getAmount() + inventory.get(i).getAmount()));
+                    markDirty();
+                    break;
+                }
+            }
+        }
+        return aliasedBlockItems;
+    }
+
+    private void replantSeed(AliasedBlockItem aliasedBlockItem, BlockPos blockPos) {
+        if(aliasedBlockItem != null) {
+            if(world.getBlockState(blockPos.down()).getBlock() instanceof FarmlandBlock && aliasedBlockItem.getBlock() instanceof CropBlock) {
+                world.setBlockState(blockPos, aliasedBlockItem.getBlock().getDefaultState(), 11);
+            }
+        }
     }
 }
